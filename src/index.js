b0b79268050781c6f9f193c66dcf584e1987a527
Alec Larson <ALarsonPi@gmail.com>
2024-11-29 11:54:34 -0500

    Maybe moving the middleware call to the service.js file will allow it to be called before the routes (and allowing it to fulfill it's purpose

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-const metricsTracker = require('./metrics.js');
 const app = require('./service.js');
 
 const port = process.argv[2] || 3000;
-app.use(metricsTracker.requestTracker);
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
